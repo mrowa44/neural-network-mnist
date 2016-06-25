21894633d9adcc2161ce59dc81cd9345b39d7469
Justyna Rachowicz <mrowa44@gmail.com>
2016-06-25 21:50:38 +0200

    Add check if guess was correct or not

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import mnist from 'mnist';
 
 const TRAINING_SIZE = 1000;
 const TEST_SIZE     = 20;
+const getRandomInt = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
 
 // Generate training and test sets
 const mnistSet    = mnist.set(TRAINING_SIZE, TEST_SIZE);
@@ -29,18 +30,44 @@ const myNetwork = new Network({
 const trainer = new Trainer(myNetwork);
 trainer.train(trainingSet, {
   rate:       0.2,
-  iterations: 50,
+  iterations: 20,
   error:      0.01,
   shuffle:    true,
   log:        1,
   cost:       Trainer.cost.CROSS_ENTROPY
 });
 
-function getRandomInt(min, max) {
-  return Math.floor(Math.random() * (max - min + 1)) + min;
-}
-
 const testElement = testSet[getRandomInt(0, TEST_SIZE)];
 const { input, output } = testElement;
-console.log(myNetwork.activate(input));
-console.log(output);
+const guess = myNetwork.activate(input);
+
+const maximum = guess.reduce((prev, curr) => Math.max(prev, curr));
+const nominators = guess.map(x => Math.exp(x - maximum));
+const denominator = nominators.reduce((prev, curr) => prev + curr);
+const softmax = nominators.map(e => e / denominator);
+
+let maxIndex = 0;
+softmax.reduce((prev, curr, i) => {
+  if (curr > prev) {
+    maxIndex = i;
+    return curr;
+  } else {
+    return prev;
+  }
+});
+
+const result = [];
+for (let i = 0; i < guess.length; i++) {
+  i === maxIndex ? result.push(1) : result.push(0);
+}
+
+const equalsOne    = elem => elem === 1;
+const guessedDigit = result.findIndex(equalsOne);
+const testDigit    = output.findIndex(equalsOne);
+
+if (guessedDigit === testDigit) {
+  console.log(`Yay! ${guessedDigit} was guessed correctly!`);
+} else {
+  console.log(`Oh no, I thought it was ${guessedDigit}, but really it was ${testDigit} :(`);
+}
+process.exit();
